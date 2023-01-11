99def91938df3d10d9148a9bda313b84cbe4c97f
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-01-10 22:09:45 -0800

    worked on JavaScript to attempt to fetch data

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 // citySearch = $('city-search').on('click',function (event)
 
 var apiKey = "8ddb894288e98c698578a2a957dbf1d5";
-var lat = "";
-var long = "";
-var requestUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}";
+// var lat = "";
+// var long = "";
+var limit = 1;
+// var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}}&appid=${apiKey}`;
 
-console.log("test log");
-fetch(requestUrl).then(function (response) {
-    console.log(response);
-    return response.json();
+// console.log("test log");
+// fetch(requestUrl).then(function (response) {
+//     console.log(response);
+//     return response.json();
+// });
+
+var button = $(".button");
+var citySearch = $("#city-search");
+
+button.on("click", function (event) {
+    event.preventDefault();
+    var cityName = citySearch[0].value;
+    console.log(cityName);
+
+    if (cityName === "") {
+        alert("Please input valid city");
+        return;
+    }
+    var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`;
+
+    fetch(geocodeUrl)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            lat = data.lat;
+            lon = data.lon;
+            console.log(lat);
+            console.log(lon);
+        });
 });
